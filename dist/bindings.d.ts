8fb2243d22e6df686ede02000d549ec539674c69
Marek Serafin <marek@snowheads.pl>
2024-07-23 16:44:07 +0200

    chore: removing dist files

--- dist/bindings.d.ts
+++
@@ -1,5 +0,0 @@
-import { EDHOC } from './edhoc';
-export interface NodeGypBinding {
-    EDHOC: EDHOC;
-}
-//# sourceMappingURL=bindings.d.ts.map
--- dist/index.d.ts
+++
@@ -1,4 +0,0 @@
-export * from "./edhoc";
-export * from "./crypto";
-export * from "./credentials";
-//# sourceMappingURL=index.d.ts.map
